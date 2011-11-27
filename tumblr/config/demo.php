8c366fdf1d5d5afecb06053bdc531a41a034e06a
Woody Gilk <woody.gilk@kohanaframework.org>
2011-11-27 13:13:24 -0600

    Added demo config files for Twitter and Tumblr

---
+++ tumblr/config/demo.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'tumblr' => array(
+		'name' => 'Tumblr',
+		'url'  => 'https://github.com/shadowhand/apis',
+	),
+);
---
+++ twitter/config/demo.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'twitter' => array(
+		'name' => 'Twitter',
+		'url'  => 'https://github.com/shadowhand/apis',
+	),
+);
